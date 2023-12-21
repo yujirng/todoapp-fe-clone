d33d1fcaca0328ba1df3d270000228e50d54d3d9
Nguyen Trong Tin <tin.nt.26oct02@gmail.com>
2023-12-21 19:50:36 +0700

    fix warning

--- src/components/todolist/icons/AddIcon.tsx
+++ src/components/todolist/icons/AddIcon.tsx
@@ -6,7 +6,7 @@ const AddIcon: FC<IIconProps> = ({ className }) => {
     <svg width="13" height="13" className={className}>
       <path
         fill="currentColor"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z"
       ></path>
     </svg>
--- src/components/todolist/icons/CloseIcon.tsx
+++ src/components/todolist/icons/CloseIcon.tsx
@@ -13,9 +13,9 @@ const CloseIcon: FC<IIconProps> = ({ className }) => {
     >
       <path
         fill="currentColor"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M11.854 11.854a.5.5 0 0 0 0-.707L8.707 8l3.147-3.146a.5.5 0 0 0-.708-.707L8 7.293 4.854 4.147a.5.5 0 1 0-.708.707L7.293 8l-3.147 3.147a.5.5 0 1 0 .708.707L8 8.708l3.146 3.146a.5.5 0 0 0 .708 0Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       ></path>
     </svg>
   );
